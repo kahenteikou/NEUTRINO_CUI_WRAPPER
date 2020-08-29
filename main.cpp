a254b183317dce1234a22685a770a91b0ab94370
kokkiemouse <kokkiemouse@gmail.com>
2020-08-29 17:36:34 +0900

    ??
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include "main.h"
+void stdout_args(int argc,char* argv[]){
+    int i;
+    for(i=0;i<argc;i++){
+        std::cout << "argv[" << i << "]=" << argv[i] << std::endl;
+    }
+
+}
 int main(int argc,char* argv[]){
-    
+    bool no_gpu=false;
+    int c;
+    const char* optstring = "i:d:zo:n";
+    const struct option longopts[] = 
+    {
+        {"musicxml",required_argument,0,'i'},
+        {"neutrino",required_argument,0,'d'},
+        {"no_gpu",optional_argument,0,'z'},
+        {"output_dir",required_argument,0,'o'},
+        {"use_nsf",optional_argument,0,'n'},
+        {0,0,0,0}
+    };
+    int lngindex=0;
+    opterr=0;
+    while ((c = getopt_long(argc,argv,optstring,longopts,&lngindex)) != -1){
+        stdout_args(argc,argv);
+        break;
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef _HEADER_NEUTRINO_CUI_WRAPPER
 #define _HEADER_NEUTRINO_CUI_WRAPPER
 // here header
+#include <getopt.h>
 #include <iostream>
 int main(int argc,char* argv[]);
+void stdout_args(int argc,char* argv[]);
 // end
 #endif
